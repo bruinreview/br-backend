659beb083473ede1c9b96c4ba9fce0623aa56eb1
Sharon <sharonbinujohn@gmail.com>
2019-09-05 22:09:35 -0700

    added createPost() mutation and posts() query, added mongo client and connection string

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+const {DateTimeResolver, URLResolver, EmailAddressResolver, JSONResolver} = require('graphql-scalars')
+const {MongoClient, ObjectID} = require('mongodb').MongoClient;
+
+const MONGO = "mongodb+srv://tech-team:BRmeMeSRkewl@website-87fht.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(MONGO, { useNewUrlParser: true });
+client.connect(err => {
+  db = client.db("website");
+  const Posts = db.collection("posts");
+  client.close();
+});
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
-  type Query {
-    hello: String
-  }
 
   type quickFacts {
     datePosted: String!
@@ -15,27 +22,76 @@ const typeDefs = gql`
   }
 
   type postHighlight {
-    userID: ID!
+    userId: ID!
     lines: String!
   }
 
  type Post {
-   Title: String!
+   _id: ID!
+   title: String!
    content: String!
-   image: String!
+   imgUrl: String!
    contentType: String!
    quickFacts: quickFacts
    isFeatured: Boolean!
 	 highlighted: [postHighlight]
 	 editedBy: String!
  }
+
+ type Query {
+   posts: [Post]
+   post(postId: ID!): Post
+ }
+
+ type Mutation {
+   createPost(title: String!, content: String!, imgUrl: String!, contentType: String!,
+     quickFacts: quickFacts, isFeatured: Boolean!, highlighted: [postHighlight], editedBy: String!): Post
+   updatePost(postId: ID!, title: String!, content: String!, imgUrl: String!, contentType: String!,
+     quickFacts: quickFacts, isFeatured: Boolean!, highlighted: [postHighlight], editedBy: String!): Post
+ }
+
 `;
+
 
 // Provide resolver functions for your schema fields
 const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
+  Query : {
+    posts: async() => {
+      return await Posts.find({}).toArray();
+    },
+
+    post: async(root, {postId}) => {
+      return await Posts.findOne({_id: postId});
+    }
+
   },
+
+ Mutation : {
+   createPost: async(root, args) => {
+      const newPost = { title: args.title, content: args.content, imgUrl: args.imgUrl,
+        contentType: args.contentType, quickFacts: args.quickFacts, isFeatured: args.isFeatured,
+        highlighted: args.highlighted, editedBy: args.editedBy};
+
+      let newlyCreatedPost = await Posts.insertOne( {newPost} );
+      try {
+        return newlyCreatedPost.ops[0];
+      }
+      catch(e) {
+        console.error(e);
+      }
+    },
+
+/*
+  updatePost: async(root, args) => {
+    const updatedPost = {_id: args.postId, title: args.title, content: args.content, imgUrl: args.imgUrl,
+      contentType: args.contentType, quickFacts: args.quickFacts, isFeatured: args.isFeatured,
+      highlighted: args.highlighted, editedBy: args.editedBy};
+
+    let newlyUpdatedPost = await Posts.findOneAndUpdate( {_id: ObjectID(args.postId)}, {updatedPost })
+
+  }
+*/
+   }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
